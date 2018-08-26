3c5d3ee73e5299c625f13ad82988809a7084939a
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-26 15:56:33 -0700

    technology styling

--- src/components/Technology/index.js
+++ src/components/Technology/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
 import './Technology.css';
 
 
@@ -7,41 +6,39 @@ class Technology extends Component {
     render() {
         return (
             <div id="Technology">
-                <Grid className="Grid">
-                    <Row className="show-grid">
-                        <h1 className="title">
-                            Full Stack Developer
-                        </h1>
-                        <Col xs="6" sm="4" >
-                            <h2 className="Bold">Frontend:</h2>
+                <h1 className="header">
+                    Full Stack Developer
+                </h1>
+                <div className="technology-container">
+                    <div className="frontend">
+                        <h2>Frontend:</h2>
                             <p>React</p>
-                            <p>JavaScript</p> 
-                            <p>JQuery</p> 
-                            <p>HTML</p>  
+                            <p>JavaScript</p>
+                            <p>JQuery</p>
+                            <p>HTML</p>
                             <p>CSS</p>
                             <p>Apollo</p>
-                        </Col>
-                        <Col xs="6" sm="4">
-                            <h2 className="Bold">Backend:</h2>
+                    </div>
+                    <div className="backend">
+                        <h2>Backend:</h2>
                             <p>Node</p>
-                            <p>Express</p> 
-                            <p>MySQL</p> 
-                            <p>MongoDB</p>    
-                        </Col>
-                        <Col xs="6" sm="4">
-                            <h2 className="Bold">Dev Tools:</h2>
+                            <p>Express</p>
+                            <p>MySQL</p>
+                            <p>MongoDB</p>
+                    </div>
+                    <div className="devtools">
+                        <h2>Dev Tools:</h2>
                             <p>Git</p>
-                            <p>Github</p> 
-                            <p>Terminal</p> 
-                            <p>Jira</p>  
+                            <p>Github</p>
+                            <p>Terminal</p>
+                            <p>Jira</p>
                             <p>Trello</p>
                             <p>Visual Studio Code</p>
-                            <p>Postman</p> 
-                            <p>GraphQL</p> 
-                            <p>Sequel Pro</p>  
-                        </Col>
-                    </Row>
-                </Grid>
+                            <p>Postman</p>
+                            <p>GraphQL</p>
+                            <p>Sequel Pro</p>
+                    </div>
+                </div>
             </div>
         )
     }
